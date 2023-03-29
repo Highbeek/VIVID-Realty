4196e29527fd08d6d42c5dfec5fece2d3fd12467
highbeek <ibukunagbo@gmail.com>
2023-03-29 11:01:53 +0100

    creating the navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import GlobalStyle from "./globalStyle";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
